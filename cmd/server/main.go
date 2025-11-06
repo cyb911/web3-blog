baf4acd943aa58a600cf124b6d35f66892e6831e
yunbin.chen <yunbin110@gmail.com>
2025-11-06 22:39:50 +0800

    gin

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "github.com/gin-gonic/gin"
 
+func main() {
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello World")
+	})
+
+	router.Any("/login", func(c *gin.Context) {
+		c.String(200, "login")
+	})
+	err := router.Run(":8080")
+	if err != nil {
+		panic(err)
+	}
 }
