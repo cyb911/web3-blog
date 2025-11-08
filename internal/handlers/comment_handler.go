82df24ef6619eb9625941a88b6422917178a03a6
yunbin.chen <yunbin110@gmail.com>
2025-11-08 21:47:59 +0800

    文章评论

--- internal/handlers/comment_handler.go
+++ internal/handlers/comment_handler.go
@@ -1,23 +1,54 @@
 package handlers
 
 import (
-	"fmt"
+	"strconv"
+	"web-blog/internal/config"
+	"web-blog/internal/middleware"
+	"web-blog/internal/models"
 	"web-blog/internal/utils"
 
 	"github.com/gin-gonic/gin"
 )
 
 type CreateCommentReq struct {
-	PostID  uint   `json:"post_id"  binding:"required"`
 	Content string `json:"content"  binding:"required"`
 }
 
 func CreateComment(c *gin.Context) {
-	utils.OkMsg(c, "待开发中")
+	postId64, err := strconv.ParseUint(c.Param("postId"), 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	postId := uint(postId64)
+
+	var req CreateCommentReq
+	if err := c.ShouldBindJSON(&req); err != nil {
+		if err.Error() == "EOF" {
+			utils.FailMsg(c, "P00001", "请填写参数！")
+		} else {
+			utils.FailMsg(c, "P00001", err.Error())
+		}
+		return
+	}
+	userId := middleware.MustGetUserID(c)
+	comment := models.Comment{
+		Content: req.Content,
+		UserId:  userId,
+		PostId:  postId,
+	}
+	if err := config.DB.Create(&comment).Error; err != nil {
+		panic(err)
+	}
+	utils.Ok(c)
 }
 
 func ListComments(c *gin.Context) {
-	postID := c.Param("postId")
-	fmt.Println(postID)
-	utils.OkMsg(c, "待开发中")
+	postID := c.Param("id")
+	var list []models.Comment
+	if err := config.DB.
+		Where("post_id = ?", postID).
+		Order("id ASC").Find(&list).Error; err != nil {
+		panic(err)
+	}
+	utils.OkData(c, list)
 }
--- internal/models/comment.go
+++ internal/models/comment.go
@@ -5,6 +5,6 @@ import "gorm.io/gorm"
 type Comment struct {
 	gorm.Model
 	Content string `json:"content" gorm:"type:text;not null"`
-	UserId  uint   `json:"user_id" gorm:"index;not null"`
-	PostId  uint   `json:"post_id" gorm:"index;not null"`
+	UserId  uint   `json:"userId" gorm:"index;not null"`
+	PostId  uint   `json:"postId" gorm:"index;not null"`
 }
